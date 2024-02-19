4e324ff4d55f9157e1c2e42c5c8cbaca5de0d4e4
Annie Yu <yua6@wit.edu>
2024-02-19 14:29:52 -0500

    Add files via upload

---
+++ SearchResult.java
@@ -0,0 +1,29 @@
+package com.skyscanner;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class SearchResult {
+    @JsonProperty
+    private String city, title, kind;
+
+    public SearchResult(){
+
+    }
+
+    public SearchResult(String city, String title, String kind){
+        this.city = city;
+        this.title = title;
+        this.kind = kind;
+    }
+
+    public String getCity(){
+        return city;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public String getKind(){
+        return kind;
+    }
+}
