b63221c987e7f130de5f4179210228d6bf025fdd
Annie Yu <yua6@wit.edu>
2024-02-19 14:33:21 -0500

    updated package

--- src/main/java/com/skyscanner/HoenScannerApplication.java
+++ src/main/java/com/skyscanner/HoenScannerApplication.java
@@ -1,5 +1,12 @@
 package com.skyscanner;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import io.dropwizard.core.Application;
 import io.dropwizard.core.setup.Bootstrap;
 import io.dropwizard.core.setup.Environment;
@@ -21,8 +28,25 @@ public class HoenScannerApplication extends Application<HoenScannerConfiguration
     }
 
     @Override
-    public void run(final HoenScannerConfiguration configuration, final Environment environment) {
-
+    public void run(final HoenScannerConfiguration configuration, final Environment environment) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        List<SearchResult> carResults = Arrays.asList(
+            mapper.readValue(
+                getClass().getClassLoader().getResource("rental_cars.json"),  
+                SearchResult[].class  
+            )
+        );
+        List<SearchResult> hotelResults = Arrays.asList(
+            mapper.readValue(
+                getClass().getClassLoader().getResource("hotels.json"),  
+                SearchResult[].class  
+            )
+        );
+        List<SearchResult> searchResults = new ArrayList<>();
+        searchResults.addAll(carResults);
+        searchResults.addAll(hotelResults);
+        final SearchResource resource = new SearchResource(searchResults);
+        environment.jersey().register(resource);
     }
 
 }
